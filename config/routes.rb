6f704fcbfe9b12a3491e72edda2a4c40b5095d9b
Ryo1916 <telemon1916@gmail.com>
2018-12-10 01:43:01 +0900

    Change routes to use generated controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    sessions: 'users/sessions'
-  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'boards#index'
 
   get 'boards/index', as: :boards
   get 'boards/new', to: "boards#new"
   post 'boards/create'
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
 end
