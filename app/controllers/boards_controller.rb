5896f0ab480b397b28ace25593ebdc7c95b15b6d
kotaro <cotaroo.y@gmail.com>
2018-12-10 11:25:14 +0900

    編集機能は完了しました！

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,6 +1,7 @@
 class BoardsController < ApplicationController
   def index
     @boards = Board.all
+    # @idhen = Board.find_by(id: paramas[:id])
   end
 
   def show
@@ -22,11 +23,22 @@ class BoardsController < ApplicationController
   end
 
   def edit
-
+    @board_edit = Board.new
+    @board = Board.find_by(id: params[:id])
   end
 
   def update
+    @board_old = Board.find_by(id: params[:id])
+    @board_old.name = params[:name]
+    @board_old.title = params[:title]
+    @board_old.body = params[:body]
 
+    if @board_old.save
+      flash[:notice] = "Created!!"
+      redirect_to("/boards/index")
+    else
+      render("/boards/new")
+    end
   end
 
   def destroy
