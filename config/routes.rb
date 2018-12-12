68a500714468a26d156e87c86f5c5cbc751001d8
AZUMA YUKI <azm0717@gmail.com>
2018-12-12 20:24:01 +0900

    ルーティング戻すで

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :boards
+  # resources :boards
+  root 'boards#index'
+  get 'boards/index', as: :boards
+  get 'boards/new', to: "boards#new"
+  get 'boards/:id/edit', to: "boards#edit"
+  post 'boards/create'
+  post 'boards/:id/update', to: "boards#update"
+  post 'boards/:id/destroy', to: "boards#destroy"
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
