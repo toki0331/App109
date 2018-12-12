9b151bb382a2fa623ede1d39001ab01a9950c41b
AZUMA YUKI <azm0717@gmail.com>
2018-12-12 09:19:51 +0900

    redefined_routes_boards_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'boards#index'
 
-  get 'boards/index', as: :boards
-  get 'boards/new', to: "boards#new"
-  get 'boards/:id/edit', to: "boards#edit"
-  post 'boards/create'
-  post 'boards/:id/update', to: "boards#update"
-  post 'boards/:id/destroy', to: "boards#destroy"
-
+  resources :boards
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
